e10276d1d78668c1cd372d98508e0a9eccb7cba7
lzhbhlrPython <Liubaolin20070125@outlook.com>
2020-08-02 12:31:00 +0800

    add fa

--- js/rainbowUI-element.js
+++ js/rainbowUI-element.js
@@ -15,6 +15,11 @@ var rainbowUI_Element = {
         popup.setAttribute('type', 'text/javascript');
         popup.setAttribute('src', 'js/rainbowUI-popup.js');
         document.head.appendChild(popup);
+        var fontAwesome = document.createElement('link');
+        fontAwesome.setAttribute('type', 'text/css');
+        fontAwesome.setAttribute('rel', 'stylesheet');
+        fontAwesome.setAttribute('href', 'css/font-awesome.min.css');
+        document.head.appendChild(fontAwesome);
     },
 
 }
