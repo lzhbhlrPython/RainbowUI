ea9a943295edf9199ead09ffef05334fbdd0ffcd
lzhbhlrPython <Liubaolin20070125@outlook.com>
2020-08-02 18:08:24 +0800

    Update rainbowUI-popup.js

--- js/rainbowUI-popup.js
+++ js/rainbowUI-popup.js
@@ -2,30 +2,28 @@ var rainbowUI_Popup = {
     version: "0.1.0",
     author: "Barry",
     license: "MIT",
-    msgbox: function msgbox(msg, title = "title", className = null) {
-        if (className != null) {
+    msgbox: function msgbox(msg, title = "title", timer = null, className = "") {
+        {
             swal(msg, {
                 title: title,
                 buttons: false,
-                className: className
-            })
-        } else {
-            swal(msg, {
-                title: title,
-                buttons: false
+                className: className,
+                timer: timer
             })
         }
     },
-    msgboxWithIcon: function msgboxWithIcon(title = "title", icon = "success") {
+    msgboxWithIcon: function msgboxWithIcon(title = "title", icon = "success", timer = null, ) {
         swal({
             title: title,
             buttons: false,
+            timer: timer,
             icon: icon
         })
     },
-    msgboxWithbutton: function msgboxWithbutton(msg, title = "title", buttonText = "button") {
+    msgboxWithClose: function msgboxWithbutton(msg, title = "title", buttonText = "button", timer = null) {
         swal(msg, {
             title: title,
+            timer: timer,
             button: buttonText
         })
 
